1103d763d16ba902a00a8fd1774d4668d6eeb6bf
Chris C Smith <chris@cu-vpn-colorado-edu-198.11.31.181.int.colorado.edu>
2019-01-18 11:16:50 -0700

    cleaned up paralog script a little

--- simulateRandomHeteros.R
+++ simulateRandomHeteros.R
@@ -1,51 +1,41 @@
 
-# example:
-sample(c(1,2), 10, replace = T) # 1 for hetero, 2 for not. If diploid, then 1/2 chance of being hetero by chance.
 
-# find a good threshold for each different number of samples covered
+# Assuming allele frequencies p=0.5 and q=0.5 to remain conservative. HWE expects 1/2 heterozygous.
+  # example:
+sample(c(1,2), 10, replace = T) # 1 for hetero, 2 for homozygous.
+
+
+
+# I want to know what proportion of heterozygotes in my sample would indicate a likely paralog.
+# Finding the proportion of heterozygotes above which there is <0.001 probability of being sampled by chance in this very basic model.
+# Therefore, I would expect the false positive rate to be about 0.001, but it should filter out many of the obvious paralogs.
 proportionYouWant = 0.001
 numReps = 100000
 propsOutput = c()
-for (s in 10:1547)
+maxSamples = 1547
+for (s in 10: maxSamples)
 {
-	numSamples = s
-	collection = rep(NA, numReps)
-	
-	# simulate numReps times
-	for (i in 1:numReps)
-	{
-		sim = sample(c(1,2), numSamples, replace = T)
-		#sim = sample(c(1,2,2,2,2,2,2,2,2,2), numSamples, replace = T)
-		count = length(which(sim==1))
-		collection[i] = count
-	}
-	
-	# find the point where the proportion you want is expected to be hetero
-	searching = T
-	currentProp = 0.99
-	while (searching == T)
-	{
-		prop = length(collection[collection > numSamples*currentProp]) / numReps
-		if (prop >= proportionYouWant)
-		{
-			print(c(s, currentProp))
-			propsOutput = c(propsOutput, currentProp)
-			searching = F
-		}
-		currentProp = currentProp - 0.01
-	}
+        numSamples = s
+        collection = rep(NA, numReps)
+        print(s)
+        
+        # simulate numReps times
+        for (i in 1:numReps)
+        {
+                sim = sample(c(1,2), numSamples, replace = T)
+                count = length(which(sim==1))
+                collection[i] = count
+        }
+        
+        sortedCollection = sort(collection)
+        threshold = sortedCollection[numReps - (proportionYouWant* numReps)] / numSamples
+        propsOutput = c(propsOutput, threshold)
 }
-numCovered = seq(10,1547)
+numCovered = seq(10, maxSamples)
 df = cbind(numCovered, propsOutput)
 df
 
 write.table(df, "paralogThresholds_0.001_1500samples.txt", sep = "\t", row.names = F, col.names = F, quote = F)
-
-
-
-
-
-
 
 
 
